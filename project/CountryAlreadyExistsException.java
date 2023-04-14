ccb7179430825edb7e6537e2f82ed1c8ef4c0b52
Sidinecyu <130146391+Sidinecyu@users.noreply.github.com>
2023-04-14 22:08:55 +0200

    Delete project directory

--- project/CountryAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package edu.webservice.project;
-
-public class CountryAlreadyExistsException  extends Exception {
-	public CountryAlreadyExistsException(Destination dest) {
-		super("Destination exists : " + dest.toString());
-	}
-}
